285ccc7e2c8d7fc38c4bfd88c4b173c9e04cb18b
Shaun Smith <shaun.smith@oracle.com>
2021-10-26 13:52:43 -0400

    initial

---
+++ candy/src/main/java/candy/RequestController.java
@@ -0,0 +1,48 @@
+package candy;
+
+import candy.RequestController.Message;
+import io.micronaut.core.annotation.Introspected;
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.*;
+
+@Controller("/")
+public class RequestController {
+
+    private static String status = "green";
+    private static Integer speed = 0;
+    private static Boolean gimme = Boolean.FALSE;
+
+    @Introspected
+    public record Message(
+        Integer speed,
+        String status,
+        Boolean gimme
+    ){
+    }
+
+    @Get(uri="/candy", produces=MediaType.APPLICATION_JSON)
+    public Message candy() {
+        gimme = Boolean.TRUE;
+        return new Message(null, status, null);
+    }
+
+    @Get(uri="/query", produces=MediaType.APPLICATION_JSON)
+    public Message query() {
+        gimme = Boolean.TRUE;
+        return new Message(speed, status, null); 
+    }
+
+    @Post(uri="/auto", consumes=MediaType.APPLICATION_JSON, produces=MediaType.TEXT_HTML)
+    public void auto(Message message) {
+        speed = message.speed;
+        System.out.println(speed);
+    }
+
+    @Post(uri="/whattodo", consumes=MediaType.APPLICATION_JSON, produces=MediaType.APPLICATION_JSON)
+    public Message whattodo(Message message) {
+        status = message.status;
+        Message response = new Message(null, null, gimme);
+        gimme = Boolean.TRUE;
+        return response;
+    }
+}
